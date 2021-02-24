2b69a40c67e054cbb213f475dcc0d3b58190bc93
Mahesh Arumugam <mahesh@theom.ai>
2021-02-23 18:28:30 -0800

    template updates

--- _examples/RoutingTreeMaintenance/internal/RoutingTreeMaintenance_impl.go
+++ _examples/RoutingTreeMaintenance/internal/RoutingTreeMaintenance_impl.go
@@ -7,6 +7,8 @@ import (
 	"net"
 	"time"
 
+	"math/rand"
+
 	p "aumahesh.com/prose/RoutingTreeMaintenance/models"
 	"github.com/dmichael/go-multicast/multicast"
 	"github.com/golang/protobuf/proto"
@@ -15,7 +17,7 @@ import (
 
 const (
 	inactivityTimeout = time.Duration(2) * time.Minute
-	heartbeatInterval = time.Duration(10) * time.Second
+	heartbeatInterval = time.Duration(1) * time.Minute
 	maxDatagramSize   = 1024
 )
 
@@ -83,7 +85,9 @@ func (this *RoutingTreeMaintenance_impl) initState() {
 	this.state.Dist = 0
 	this.state.Inv = 0
 	this.state.P = ""
-	this.state.Up = false
+
+	this.state.Dist = this.initVaribleDist()
+	this.state.Inv = this.initVaribleInv()
 
 }
 
@@ -99,6 +103,22 @@ func (this *RoutingTreeMaintenance_impl) initConstantNULL() int64 {
 	temp1 := int64(0)
 
 	return temp1
+}
+
+func (this *RoutingTreeMaintenance_impl) initVaribleDist() int64 {
+
+	temp3 := int64(10)
+
+	temp4 := rand.Int63n(temp3)
+
+	return temp4
+}
+
+func (this *RoutingTreeMaintenance_impl) initVaribleInv() int64 {
+
+	temp2 := int64(0)
+
+	return temp2
 }
 
 func (this *RoutingTreeMaintenance_impl) EventHandler(ctx context.Context) {
@@ -120,20 +140,22 @@ func (this *RoutingTreeMaintenance_impl) EventHandler(ctx context.Context) {
 				Dist: s.State.Dist,
 				Inv:  s.State.Inv,
 				P:    s.State.P,
-				Up:   s.State.Up,
 			}
 			this.neighborState[s.Id].updatedAt = time.Now()
 			this.evaluateNeighborStates()
 			this.updateLocalState()
-			_, err := this.broadcastLocalState()
-			if err != nil {
-				log.Errorf("Error broadcasting local state to neighbors")
+			if stateChanged := this.updateLocalState(); stateChanged {
+				this.broadcastLocalState()
+				if err != nil {
+					log.Errorf("Error broadcasting local state to neighbors")
+				}
 			}
 		case s := <-this.hbChannel:
 			_, ok := this.neighborState[s.Id]
 			if !ok {
 				this.neighborState[s.Id] = &NeighborState{
 					id:             s.Id,
+					state:          &p.State{},
 					discoveredAt:   time.Now(),
 					active:         true,
 					stateChangedAt: time.Now(),
@@ -144,6 +166,9 @@ func (this *RoutingTreeMaintenance_impl) EventHandler(ctx context.Context) {
 			this.evaluateNeighborStates()
 			if stateChanged := this.updateLocalState(); stateChanged {
 				this.broadcastLocalState()
+				if err != nil {
+					log.Errorf("Error broadcasting local state to neighbors")
+				}
 			}
 		case <-heartbeatTicker.C:
 			this.sendHeartBeat()
@@ -194,35 +219,39 @@ func (this *RoutingTreeMaintenance_impl) setNeighbor(id string, state bool) bool
 func (this *RoutingTreeMaintenance_impl) doAction0() bool {
 	stateChanged := false
 
+	log.Debugf("Executing: doAction0")
+
 	var found bool
 	var neighbor *NeighborState
 	for _, neighbor = range this.neighborState {
-		temp2 := neighbor.state.Dist
-		temp3 := this.state.Dist
-		temp4 := temp2 < temp3
-		temp5 := neighbor.id
-		temp6 := this.isNeighborUp(temp5)
-		temp7 := neighbor.state.Inv
-		temp8 := CMAX
-		temp9 := temp7 < temp8
+		temp5 := neighbor.state.Dist
+		temp6 := this.state.Dist
+		temp7 := temp5 < temp6
+		temp8 := neighbor.id
+		temp9 := this.isNeighborUp(temp8)
 		temp10 := neighbor.state.Inv
-		temp11 := this.state.Inv
+		temp11 := CMAX
 		temp12 := temp10 < temp11
-		temp13 := temp9 && temp12
-		temp14 := temp6 && temp13
-		temp15 := temp4 && temp14
-		if temp15 {
+		temp13 := neighbor.state.Inv
+		temp14 := this.state.Inv
+		temp15 := temp13 < temp14
+		temp16 := temp12 && temp15
+		temp17 := temp9 && temp16
+		temp18 := temp7 && temp17
+		if temp18 {
 			found = true
 			break
 		}
 	}
 	if found {
-		temp16 := neighbor.id
-		this.state.P = temp16
-		temp17 := neighbor.state.Inv
-		this.state.Inv = temp17
+		temp19 := neighbor.id
+		this.state.P = temp19
+		temp20 := neighbor.state.Inv
+		this.state.Inv = temp20
 		stateChanged = true
 	}
+
+	log.Debugf("doAction0: state changed: %v", stateChanged)
 
 	return stateChanged
 }
@@ -230,41 +259,45 @@ func (this *RoutingTreeMaintenance_impl) doAction0() bool {
 func (this *RoutingTreeMaintenance_impl) doAction1() bool {
 	stateChanged := false
 
+	log.Debugf("Executing: doAction1")
+
 	var found bool
 	var neighbor *NeighborState
 	for _, neighbor = range this.neighborState {
-		temp18 := neighbor.state.Dist
-		temp19 := this.state.Dist
-		temp20 := temp18 < temp19
-		temp21 := neighbor.id
-		temp22 := this.isNeighborUp(temp21)
-		temp23 := neighbor.state.Inv
-		temp24 := int64(1)
-		temp25 := temp23 + temp24
-		temp26 := CMAX
-		temp27 := temp25 < temp26
-		temp28 := neighbor.state.Inv
-		temp29 := int64(1)
-		temp30 := temp28 + temp29
-		temp31 := this.state.Inv
-		temp32 := temp30 < temp31
-		temp33 := temp27 && temp32
-		temp34 := temp22 && temp33
-		temp35 := temp20 && temp34
-		if temp35 {
+		temp21 := neighbor.state.Dist
+		temp22 := this.state.Dist
+		temp23 := temp21 < temp22
+		temp24 := neighbor.id
+		temp25 := this.isNeighborUp(temp24)
+		temp26 := neighbor.state.Inv
+		temp27 := int64(1)
+		temp28 := temp26 + temp27
+		temp29 := CMAX
+		temp30 := temp28 < temp29
+		temp31 := neighbor.state.Inv
+		temp32 := int64(1)
+		temp33 := temp31 + temp32
+		temp34 := this.state.Inv
+		temp35 := temp33 < temp34
+		temp36 := temp30 && temp35
+		temp37 := temp25 && temp36
+		temp38 := temp23 && temp37
+		if temp38 {
 			found = true
 			break
 		}
 	}
 	if found {
-		temp36 := neighbor.id
-		this.state.P = temp36
-		temp37 := neighbor.state.Inv
-		temp38 := int64(1)
-		temp39 := temp37 + temp38
-		this.state.Inv = temp39
+		temp39 := neighbor.id
+		this.state.P = temp39
+		temp40 := neighbor.state.Inv
+		temp41 := int64(1)
+		temp42 := temp40 + temp41
+		this.state.Inv = temp42
 		stateChanged = true
 	}
+
+	log.Debugf("doAction1: state changed: %v", stateChanged)
 
 	return stateChanged
 }
@@ -272,76 +305,80 @@ func (this *RoutingTreeMaintenance_impl) doAction1() bool {
 func (this *RoutingTreeMaintenance_impl) doAction2() bool {
 	stateChanged := false
 
+	log.Debugf("Executing: doAction2")
+
 	var found bool
 	var neighbor *NeighborState
 	for _, neighbor = range this.neighborState {
-		temp40 := this.state.P
-		temp41 := ""
-		temp42 := temp40 != temp41
 		temp43 := this.state.P
-		temp44 := this.isNeighborUp(temp43)
-		temp45 := false
-		temp46 := temp44 == temp45
-		var temp47 int64
-		if neighbor.id == this.state.P {
-			temp47 = this.state.Inv
-		} else {
-			continue
-		}
-		temp48 := CMAX
-		temp49 := temp47 >= temp48
+		temp44 := ""
+		temp45 := temp43 != temp44
+		temp46 := this.state.P
+		temp47 := this.isNeighborUp(temp46)
+		temp48 := false
+		temp49 := temp47 == temp48
 		var temp50 int64
 		if neighbor.id == this.state.P {
-			temp50 = this.state.Dist
+			temp50 = this.state.Inv
 		} else {
 			continue
 		}
-		temp51 := this.state.Dist
-		temp52 := temp50 < temp51
-		temp53 := this.state.Inv
-		var temp54 int64
+		temp51 := CMAX
+		temp52 := temp50 >= temp51
+		var temp53 int64
 		if neighbor.id == this.state.P {
-			temp54 = this.state.Inv
+			temp53 = this.state.Dist
 		} else {
 			continue
 		}
-		temp55 := temp53 != temp54
-		temp56 := temp52 && temp55
+		temp54 := this.state.Dist
+		temp55 := temp53 < temp54
+		temp56 := this.state.Inv
 		var temp57 int64
 		if neighbor.id == this.state.P {
-			temp57 = this.state.Dist
+			temp57 = this.state.Inv
 		} else {
 			continue
 		}
-		temp58 := this.state.Dist
-		temp59 := temp57 > temp58
-		temp60 := this.state.Inv
-		var temp61 int64
+		temp58 := temp56 != temp57
+		temp59 := temp55 && temp58
+		var temp60 int64
 		if neighbor.id == this.state.P {
-			temp61 = this.state.Inv
+			temp60 = this.state.Dist
 		} else {
 			continue
 		}
-		temp62 := int64(1)
-		temp63 := temp61 + temp62
-		temp64 := temp60 != temp63
-		temp65 := temp59 && temp64
-		temp66 := temp56 || temp65
-		temp67 := temp49 || temp66
-		temp68 := temp46 || temp67
-		temp69 := temp42 && temp68
-		if temp69 {
+		temp61 := this.state.Dist
+		temp62 := temp60 > temp61
+		temp63 := this.state.Inv
+		var temp64 int64
+		if neighbor.id == this.state.P {
+			temp64 = this.state.Inv
+		} else {
+			continue
+		}
+		temp65 := int64(1)
+		temp66 := temp64 + temp65
+		temp67 := temp63 != temp66
+		temp68 := temp62 && temp67
+		temp69 := temp59 || temp68
+		temp70 := temp52 || temp69
+		temp71 := temp49 || temp70
+		temp72 := temp45 && temp71
+		if temp72 {
 			found = true
 			break
 		}
 	}
 	if found {
-		temp70 := ""
-		this.state.P = temp70
-		temp71 := CMAX
-		this.state.Inv = temp71
+		temp73 := ""
+		this.state.P = temp73
+		temp74 := CMAX
+		this.state.Inv = temp74
 		stateChanged = true
 	}
+
+	log.Debugf("doAction2: state changed: %v", stateChanged)
 
 	return stateChanged
 }
@@ -349,18 +386,22 @@ func (this *RoutingTreeMaintenance_impl) doAction2() bool {
 func (this *RoutingTreeMaintenance_impl) doAction3() bool {
 	stateChanged := false
 
-	temp72 := this.state.P
-	temp73 := ""
-	temp74 := temp72 == temp73
-	temp75 := this.state.Inv
-	temp76 := CMAX
-	temp77 := temp75 < temp76
-	temp78 := temp74 && temp77
-	if temp78 {
-		temp79 := CMAX
-		this.state.Inv = temp79
+	log.Debugf("Executing: doAction3")
+
+	temp75 := this.state.P
+	temp76 := ""
+	temp77 := temp75 == temp76
+	temp78 := this.state.Inv
+	temp79 := CMAX
+	temp80 := temp78 < temp79
+	temp81 := temp77 && temp80
+	if temp81 {
+		temp82 := CMAX
+		this.state.Inv = temp82
 		stateChanged = true
 	}
+
+	log.Debugf("doAction3: state changed: %v", stateChanged)
 
 	return stateChanged
 }
@@ -396,7 +437,6 @@ func (this *RoutingTreeMaintenance_impl) broadcastLocalState() (int, error) {
 			Dist: this.state.Dist,
 			Inv:  this.state.Inv,
 			P:    this.state.P,
-			Up:   this.state.Up,
 		},
 	}
 	broadcastMessage := &p.BroadcastMessage{
